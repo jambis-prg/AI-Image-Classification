a77c7b199eb3742b3e226774eae3b09a85641ac1
João Victor <98539736+jambis-prg@users.noreply.github.com>
2024-09-05 19:30:48 -0300

    Modelo básico de IA convolucional
    Essa ia consegue classificar as imagens com cerca de 70% de acurácia

---
+++ main.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import multiprocessing
+
+tf.config.threading.set_intra_op_parallelism_threads(multiprocessing.cpu_count())
+tf.config.threading.set_inter_op_parallelism_threads(multiprocessing.cpu_count())
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=30, validation_split=0.2)
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print(f"Test Loss: {test_loss}, Test Acc: {test_acc}")
